2a02881fea8613ecc618de33b9e4563669770c44
srsiegel <steven.siegel@ufl.edu>
2020-11-09 15:24:48 -0500

    Button functionality to route login/reg pg to home

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,33 +1,41 @@
 import React from "react";
 import { Button, Form } from 'react-bootstrap';
 import './Login.css'
-export default function Login() {
-  return (
-    <div className="login">
-        <Form className = "loginForm">
-            <h1 className="text-center">
-                <span classname="font-weight-bold">
-                    Agora
-                </span>
-            </h1>
-            <Form.Group>
-                <Form.Label>
-                    Email
-                </Form.Label>
-                <Form.Control type="email" placeholder="Enter e-mail address" />
-            </Form.Group>
+export default class Login extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    click(){
+        window.location = "/";
+    }
+    render(){
+        return (
+            <div className="login">
+                <Form className = "loginForm">
+                    <h1 className="text-center">
+                        <span classname="font-weight-bold">
+                            Agora
+                        </span>
+                    </h1>
+                    <Form.Group>
+                        <Form.Label>
+                            Email
+                        </Form.Label>
+                        <Form.Control type="email" placeholder="Enter e-mail address" />
+                    </Form.Group>
 
-            <Form.Group>
-                <Form.Label>
-                    Password
-                </Form.Label>
-                <Form.Control type ="password" placeholder="Password" />
-            </Form.Group>
-            
-            <Button>
-                Log in
-            </Button>
-        </Form>
-    </div>
-  );
+                    <Form.Group>
+                        <Form.Label>
+                            Password
+                        </Form.Label>
+                        <Form.Control type ="password" placeholder="Password" />
+                    </Form.Group>
+                    
+                    <Button onClick={this.click}>
+                        Log in
+                    </Button>
+                </Form>
+            </div>
+        );
+    }
 }
