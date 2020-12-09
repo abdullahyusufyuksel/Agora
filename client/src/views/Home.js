4ebe50015d3b3a020913c09cf48e1ce31263f989
Ernest Ardon <ErnieA@Ernests-MBP.attlocal.net>
2020-12-08 22:08:16 -0500

    Home page functionality

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,186 +1,202 @@
 import { Jumbotron, Button, Card, CardDeck, Navbar, NavDropdown, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Component } from 'react';
+import {withRouter} from 'react-router-dom';
 import React from "react";
 import "./Home.css"
+import Axios from 'axios';
 
-const Home = (props) => {
+export class Home extends Component {
+  constructor(props) {
+    super(props);
 
-  const userData = props.currentUser.data
-  let userName;
+    this.state = {
+        "userPost" : [] ,
+        defaultNew : true 
+    }
+}
 
-  if (userData === null) {
-    userName = "not logged in"
-  } else {
-    userName = userData.data.username
+  handleSelect = (e) => {
+   
+    switch(parseInt(e)) {
+      case 1 : {
+        if (this.state.defaultNew === false)
+        {
+       
+       this.setState({ userPost: this.state.userPost.sort((a, b) => {
+          return new Date(a.date) - new Date(b.date)
+      })})
+        
+        this.setState({defaultNew: true})
+        }
+          break;
+      }
+      case 2 : {
+        
+          if (this.state.defaultNew === true)
+          {
+          this.setState({userPost: this.state.userPost.sort((a,b) => b.upvotes - a.upvotes)})
+          this.setState({defaultNew: false})
+          }
+        
+          break;
+      }
+      default : {
+       
+    }
+   
   }
+}
 
 
-  return (
+nextPath(path) {
+  this.props.history.push(path);
+}
+
+componentDidMount(){
+  
+  Axios.get(`http://localhost:5000/`)
+  .then(res => {
+    this.setState({userPost : res.data})
+    this.setState({ userPost: this.state.userPost.sort((a, b) => {
+      return new Date(a.date) - new Date(b.date)
+  })})
+   
+  })
+}
+
+  render() { 
+    
+    const userData = this.props.currentUser.data
+    //const userPostLen = this.state.userPost.length
+    let header, hMessage, hButton1, hButton2, link1, link2,rHeader1,rMessage1,rHeader2,rMessage2;
+   
+
+    if (userData === null) {
+      header = ""
+      hButton1 = "Sign Up"
+      hButton2 = "Login"
+      link1 = "/register"
+      link2 = "/login"
+      hMessage = "Share and fact-check advertisements and posts related to concurrent social topics today!"
+      rHeader1 =""
+      rHeader2 = ""
+      rMessage1= ""
+      rMessage2=""
+  } else {
+      header = "Welcome, " + userData.data.username
+      hButton1 = ""
+      hButton2 = ""
+      link1 = ""
+      link2 = ""
+      hMessage = "See resources below on information on how to stay safe during covid "
+      rHeader1 ="When to Quarantine:"
+      rHeader2 = "CDC testing information:"
+      rMessage1= "https://www.cdc.gov/coronavirus/2019-ncov/more/scientific-brief-options-to-reduce-quarantine.html"
+      rMessage2="https://www.cdc.gov/coronavirus/2019-ncov/symptoms-testing/testing.html"
+
+  }
+ 
+ 
+    return (
     <div className="Home">
       <Jumbotron>
-  <h1>Agora {userName}</h1>
+  <h1>{header}</h1>
+        <h4>
+          {hMessage}
+        </h4>
         <p>
-          Share and fact-check advertisements and posts related to concurrent social topics today!
-        </p>
-        <p>
-          <LinkContainer to="/register">
-            <Button variant="primary">Sign Up</Button>
+         
+          <LinkContainer to={link1}>
+    <Button variant="primary">{hButton1}</Button>
           </LinkContainer>
         </p>
+         
+    <h5>{rHeader1} </h5>
+        <h6>{rMessage1} </h6>
+
+        <p>
+        <LinkContainer to={link2}>
+    <Button variant="primary">{hButton2}</Button>
+          </LinkContainer>
+        </p>
+        
+        <h5>{rHeader2} </h5>
+        <h6>  {rMessage2} </h6>
+      
       </Jumbotron>
       <Navbar bg="light" expand="lg">
       <Navbar.Brand href="#home">Current Posts</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav class="justify-center-right">
-          <NavDropdown title="Sort By" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Social Topics</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">New</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Trending</NavDropdown.Item>
+          <NavDropdown title="Sort By" onSelect={ (e)=> this.handleSelect(e)}  id="basic-nav-dropdown">
+            <NavDropdown.Item eventKey={1}>New</NavDropdown.Item>
+            <NavDropdown.Item eventKey={2}>Trending</NavDropdown.Item>
           </NavDropdown>
         </Nav>
       
       </Navbar.Collapse>
     </Navbar>
+    
     <CardDeck className="Deck">
+     {this.state.userPost.slice(0,4).map( (post) => {
+        return (
       <Card style={{ width: '22rem' }}>
-        <Card.Img variant="top" src="https://media.9news.com/assets/CCT/images/f91f601a-aa7e-485d-83a0-34ef17bdd6ef/f91f601a-aa7e-485d-83a0-34ef17bdd6ef_1920x1080.jpg" />
+        <Card.Img variant="top" src={post.postMediaFilePath} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+    <Card.Title>{post.title} </Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            {post.message}
           </Card.Text>
-          <Button variant="primary">View Post</Button>
+          <Button onClick={() => this.nextPath(`/post/${post._id}`) } variant="primary">View Post</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://cdn.cnn.com/cnnnext/dam/assets/200622104651-black-lives-matter-support-impact-0613-large-169.jpg" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">View Post</Button>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQhNa4eT025_r0HKkEz41Z0USG0Av0o9XMMfw&usqp=CAU" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">View Post</Button>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://static.dw.com/image/52163054_303.jpg" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">View Post</Button>
-        </Card.Body>
-      </Card>
+       )
+       
+      })
+     }
     </CardDeck>
+    
     <CardDeck className="Deck">
+    {this.state.userPost.slice(4,8).map( (post) => {
+        return (
       <Card style={{ width: '22rem' }}>
-        <Card.Img variant="top" src="https://media.9news.com/assets/CCT/images/f91f601a-aa7e-485d-83a0-34ef17bdd6ef/f91f601a-aa7e-485d-83a0-34ef17bdd6ef_1920x1080.jpg" />
+        <Card.Img variant="top" src={post.postMediaFilePath} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+    <Card.Title>{post.title} </Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            {post.message}
           </Card.Text>
-          <Button variant="primary">View Post</Button>
+          <Button onClick={() => this.nextPath(`/post/${post._id}`) } variant="primary">View Post</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://cdn.cnn.com/cnnnext/dam/assets/200622104651-black-lives-matter-support-impact-0613-large-169.jpg" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">View Post</Button>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQhNa4eT025_r0HKkEz41Z0USG0Av0o9XMMfw&usqp=CAU" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">View Post</Button>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://static.dw.com/image/52163054_303.jpg" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">View Post</Button>
-        </Card.Body>
-      </Card>
+       )
+       
+      })
+     }
     </CardDeck>
+
     <CardDeck className="Deck">
+    {this.state.userPost.slice(8,12).map( (post) => {
+        return (
       <Card style={{ width: '22rem' }}>
-        <Card.Img variant="top" src="https://media.9news.com/assets/CCT/images/f91f601a-aa7e-485d-83a0-34ef17bdd6ef/f91f601a-aa7e-485d-83a0-34ef17bdd6ef_1920x1080.jpg" />
+        <Card.Img variant="top" src={post.postMediaFilePath} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+    <Card.Title>{post.title} </Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            {post.message}
           </Card.Text>
-          <Button variant="primary">View Post</Button>
+          <Button onClick={() => this.nextPath(`/post/${post._id}`) } variant="primary">View Post</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://cdn.cnn.com/cnnnext/dam/assets/200622104651-black-lives-matter-support-impact-0613-large-169.jpg" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">View Post</Button>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQhNa4eT025_r0HKkEz41Z0USG0Av0o9XMMfw&usqp=CAU" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">View Post</Button>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://static.dw.com/image/52163054_303.jpg" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">View Post</Button>
-        </Card.Body>
-      </Card>
+       )
+       
+      })
+     }   
     </CardDeck>
     </div>
-  );
+    )
+  }
 }
-export default Home;
+export default withRouter(Home);
