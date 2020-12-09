619564a48b0d769d2f7e4e9b159cabb0ceacce80
cameron-morrongiello <cammorrongiello@gmail.com>
2020-12-09 11:32:52 -0700

    test push to deploy

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const config = require('../config/config.js');
+const config = require('./config/config.js');
 const morgan = require('morgan');
-const Router = require('./Router.js');
+const Router = require('./server/Router.js');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 // Logging
 app.use(morgan('dev'));
@@ -28,13 +29,19 @@ mongoose.connect(config.URI, { useNewUrlParser: true, useUnifiedTopology: true }
 app.use(passport.initialize());
 
 // Pass in global config for passport
-require('../config/passport.js')(passport);
+require('./config/passport.js')(passport);
 
 // Router for everything
 app.use('/', Router);
 
-app.listen(process.env.PORT || 5000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
 
-  
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
